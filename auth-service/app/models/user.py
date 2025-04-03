2c823337b56a4962ef04842cdc3f705a6e21a82c
ekrsw <eisuke.koresawa@gmail.com>
2025-04-03 21:50:48 +0900

    tablename

--- auth-service/app/models/user.py
+++ auth-service/app/models/user.py
@@ -4,6 +4,7 @@ from app.db.base import Base
 import uuid
 
 class User(Base):
+    __tablename__ = "users"
     id: Mapped[str] = mapped_column(String, primary_key=True, index=True, default=str(uuid.uuid4()))
     username: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
