aca1b9100be327d2c13809552522fed03965bda0
ekrsw <eisuke.koresawa@gmail.com>
2025-04-03 10:47:11 +0900

    sqlit -> postgresql

--- auth-service/app/core/config.py
+++ auth-service/app/core/config.py
@@ -3,7 +3,16 @@ from pydantic import ConfigDict
 
 class Settings(BaseSettings):
     # データベース設定
-    DATABASE_URL: str = "sqlite+aiosqlite:///test.db"
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_DB: str
+    
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
     SQLALCHEMY_ECHO: bool = False  # SQLAlchemyのログ出力設定を追加
 
     model_config = ConfigDict(
