8804d254e86b0c13b486e255d5b1120a4e8845ca
ekrsw <eisuke.koresawa@gmail.com>
2025-04-04 21:06:20 +0900

    docker-compose error

--- auth-service/docker/scripts/entrypoint.sh
+++ auth-service/docker/scripts/entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # 環境変数からホストとポートを取得（デフォルト値を設定）
-HOST=${API_HOST:-"0.0.0.0"}
-PORT=${API_PORT:-"8000"}
+HOST=${AUTH_HOST:-"0.0.0.0"}
+PORT=${AUTH_PORT:-"8000"}
 
 # ユーザーappuserとして実行
 exec su -s /bin/bash appuser -c "uvicorn app.main:app --host $HOST --port $PORT --workers 2"
