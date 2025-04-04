572d319964fd06e7c361d5d78ceac6d723a61737
ekrsw <eisuke.koresawa@gmail.com>
2025-04-04 23:57:06 +0900

    check-point

--- auth-service/docker/scripts/entrypoint.sh
+++ auth-service/docker/scripts/entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # 環境変数からホストとポートを取得（デフォルト値を設定）
-HOST=${AUTH_HOST:-"0.0.0.0"}
-PORT=${AUTH_PORT:-"8000"}
+HOST=${API_HOST:-"0.0.0.0"}
+PORT=${API_PORT:-"8000"}
 
 # 直接uvicornを実行
 exec uvicorn app.main:app --host $HOST --port $PORT --workers 2
