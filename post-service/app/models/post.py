9f330046fc4ed0274c0536b64dde232eca33bae4
ekrsw <eisuke.koresawa@gmail.com>
2025-04-10 08:55:31 +0900

    M

--- post-service/app/models/post.py
+++ post-service/app/models/post.py
@@ -1,11 +1,19 @@
-from sqlalchemy import String, ForeignKey, Text
+from sqlalchemy import String, Text, Boolean, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
 from app.db.base import Base
-
+import uuid
+from datetime import datetime
 
 class Post(Base):
     __tablename__ = "posts"
+    
+    # 既存フィールド
     title: Mapped[str] = mapped_column(String, nullable=False)
     content: Mapped[str] = mapped_column(Text, nullable=True)
-    
+    user_id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), nullable=False, index=True
+        )
+    is_published: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    published_at: Mapped[datetime] = mapped_column(DateTime, nullable=True, server_default=func.now())
